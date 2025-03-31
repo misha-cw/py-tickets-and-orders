33173ccc1d48d26e9023f54fc87240e6945da401
misha-cw <bigun.mykhaylo@gmail.com>
2025-04-01 02:10:16 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -17,7 +22,7 @@ class Actor(models.Model):
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, db_index=True, unique=True)
     description = models.TextField()
     actors = models.ManyToManyField(to=Actor, related_name="movies")
     genres = models.ManyToManyField(to=Genre, related_name="movies")
@@ -50,3 +55,59 @@ class MovieSession(models.Model):
 
     def __str__(self) -> str:
         return f"{self.movie.title} {str(self.show_time)}"
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="orders")
+
+    def __str__(self) -> str:
+        return f"{datetime.strftime(self.created_at, '%Y-%m-%d %H:%M:%S')}"
+
+
+    class Meta:
+        ordering = ["-created_at"]
+
+class Ticket(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="tickets")
+    movie_session = models.ForeignKey(MovieSession, on_delete=models.CASCADE, related_name="tickets")
+    row = models.IntegerField()
+    seat = models.IntegerField()
+
+    def __str__(self) -> str:
+        return (f"{self.movie_session.movie.title}"
+                f" {datetime.strftime(self.movie_session.show_time, '%Y-%m-%d %H:%M:%S')} "
+                f"(row: {self.row}, seat: {self.seat})")
+
+    def clean(self) -> None:
+        if self.row < 1 or self.row > self.movie_session.cinema_hall.rows:
+            raise ValidationError(
+                {"row": [
+                    f"row number must be in available range:"
+                    f" (1, rows): (1, {self.movie_session.cinema_hall.rows})"
+                ]}
+            )
+
+        if self.seat < 1 or self.seat > self.movie_session.cinema_hall.seats_in_row:
+            raise ValidationError(
+                {"seat": [
+                    f"seat number must be in available range:"
+                    f" (1, seats_in_row): (1, {self.movie_session.cinema_hall.seats_in_row})"
+                ]}
+            )
+
+    def save(self, *args, **kwargs) -> None:
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["movie_session", "row", "seat"],
+                name="unique_ticket"
+            ),
+        ]
+
+
+class User(AbstractUser):
+    pass
---
+++ services/order.py
@@ -0,0 +1,40 @@
+from django.contrib.auth import get_user_model
+from django.db import transaction
+from django.db.models import QuerySet
+
+from db.models import Order, Ticket, MovieSession
+
+
+@transaction.atomic
+def create_order(
+        tickets: list[dict],
+        username: str,
+        date: str = None,
+) -> Order:
+    user = get_user_model().objects.get(username=username)
+
+    order = Order.objects.create(
+        user=user
+    )
+
+    if date:
+        get_orders(username).update(
+            created_at=date
+        )
+
+    order.refresh_from_db()
+
+    Ticket.objects.bulk_create(Ticket(
+        order_id=order.pk,
+        movie_session=MovieSession.objects.get(id=ticket["movie_session"]),
+        row=ticket["row"],
+        seat=ticket["seat"]
+    ) for ticket in tickets)
+
+    return order
+
+
+def get_orders(username: str = None) -> QuerySet[Order]:
+    if username:
+        return Order.objects.filter(user__username=username)
+    return Order.objects.all()
